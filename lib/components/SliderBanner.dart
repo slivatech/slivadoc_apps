3d1e903a953bd81c35536be13913de83824a242f
evans2101 <evans.moris21@gmail.com>
2024-03-19 02:43:06 +0700

    add SliderBanner Penawaran Menarik

---
+++ lib/components/SliderBanner.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+class SliderBanner extends StatelessWidget {
+  const SliderBanner({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Column(
+        children: [
+          TabBar(
+            labelStyle: const TextStyle(fontSize: 11, color: Colors.white),
+            unselectedLabelStyle: const TextStyle(
+                fontSize: 10, color: Color.fromARGB(255, 20, 123, 219)),
+            indicator: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            indicatorPadding: const EdgeInsets.symmetric(vertical: 5),
+            tabs: [
+              Tab(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Align(
+                      alignment: Alignment.center,
+                      child: Text('Semua'),
+                    ),
+                  ),
+                ),
+              ),
+              Tab(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Align(
+                      alignment: Alignment.center,
+                      child: Text('Promo'),
+                    ),
+                  ),
+                ),
+              ),
+              Tab(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Align(
+                      alignment: Alignment.center,
+                      child: Text('Kerja Sama'),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10.0),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.21,
+            child: TabBarView(
+              children: [
+                _buildSlider(
+                  context,
+                  [
+                    'lib/assets/promo.png',
+                    'lib/assets/promo1.png',
+                    'lib/assets/promo2.png',
+                    'lib/assets/promo3.png',
+                    'lib/assets/partnership.png',
+                    'lib/assets/partner1.png',
+                    'lib/assets/partner2.png',
+                    'lib/assets/apps.png',
+                  ],
+                ),
+                _buildSlider(
+                  context,
+                  [
+                    'lib/assets/promo.png',
+                    'lib/assets/promo1.png',
+                    'lib/assets/promo2.png',
+                    'lib/assets/promo3.png',
+                  ],
+                ),
+                _buildSlider(context, [
+                  'lib/assets/partnership.png',
+                  'lib/assets/partner1.png',
+                  'lib/assets/partner2.png',
+                  'lib/assets/apps.png',
+                ]),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSlider(BuildContext context, List<String> imageUrls) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: imageUrls.map((imageUrl) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Container(
+              width: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: AssetImage(imageUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:slivadoc_mobile_apps/screens/CariDokterPage.dart';
 import 'package:slivadoc_mobile_apps/screens/TanyaDokterPage.dart';
 import 'package:slivadoc_mobile_apps/screens/ArtikelKesehatanPage.dart';
-// import 'package:slivadoc_mobile_apps/screens/SlivaProteksiPage.dart';
 import 'package:slivadoc_mobile_apps/screens/SlivaShopPage.dart';
 import 'package:slivadoc_mobile_apps/screens/AnimalEventPage.dart';
 import 'package:slivadoc_mobile_apps/screens/AnimalAcademyPage.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:slivadoc_mobile_apps/screens/Profile.dart';
+import 'package:slivadoc_mobile_apps/components/SliderBanner.dart';
 
 void main() {
   runApp(MyApp());
@@ -63,100 +63,101 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: Colors.white,
-          elevation: 0,
-          title: Row(
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: TextFormField(
-                      style: const TextStyle(fontSize: 14, color: Colors.grey),
-                      decoration: const InputDecoration(
-                        hintText: 'Cari nama dokter, klinik?',
-                        border: InputBorder.none,
-                        prefixIcon:
-                            Icon(Icons.search, color: Color(0xFF3254AB)),
-                      ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextFormField(
+                    style: const TextStyle(fontSize: 14, color: Colors.grey),
+                    decoration: const InputDecoration(
+                      hintText: 'Cari nama dokter, klinik?',
+                      border: InputBorder.none,
+                      prefixIcon:
+                          Icon(Icons.search, color: Color(0xFF3254AB)),
                     ),
                   ),
                 ),
               ),
-              IconButton(
-                onPressed: () {
-                  // Handle notification icon press
-                },
-                icon: const Icon(
-                  Icons.notifications,
-                  color: Color(0xFF3254AB),
-                ),
+            ),
+            IconButton(
+              onPressed: () {
+                // Handle notification icon press
+              },
+              icon: const Icon(
+                Icons.notifications,
+                color: Color(0xFF3254AB),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: _pageMap.values.elementAt(_currentIndex),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(255, 120, 247, 252),
+              blurRadius: 3.0,
+              spreadRadius: 1.0,
+              offset: Offset(0, -1),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+          child: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: onTabTapped,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home, color: Color(0xFF3254AB)),
+                label: 'Beranda',
+              ),
+              BottomNavigationBarItem(
+                icon: FaIcon(FontAwesomeIcons.stethoscope,
+                    color: Color(0xFF3254AB)),
+                label: 'Buat Janji',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.email, color: Color(0xFF3254AB)),
+                label: 'Pesan',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.article, color: Color(0xFF3254AB)),
+                label: 'Transaksi',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person, color: Color(0xFF3254AB)),
+                label: 'Profil',
               ),
             ],
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: Colors.grey,
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            elevation: 0,
+            backgroundColor: Colors.white,
           ),
         ),
-        body: _pageMap.values.elementAt(_currentIndex),
-        bottomNavigationBar: Container(
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20.0),
-              topRight: Radius.circular(20.0),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromARGB(255, 120, 247, 252),
-                blurRadius: 3.0,
-                spreadRadius: 1.0,
-                offset: Offset(0, -1),
-              ),
-            ],
-          ),
-          child: ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(20.0),
-              topRight: Radius.circular(20.0),
-            ),
-            child: BottomNavigationBar(
-              currentIndex: _currentIndex,
-              onTap: onTabTapped,
-              items: const [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home, color: Color(0xFF3254AB)),
-                  label: 'Beranda',
-                ),
-                BottomNavigationBarItem(
-                  icon: FaIcon(FontAwesomeIcons.stethoscope,
-                      color: Color(0xFF3254AB)),
-                  label: 'Buat Janji',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.email, color: Color(0xFF3254AB)),
-                  label: 'Pesan',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.article, color: Color(0xFF3254AB)),
-                  label: 'Transaksi',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.person, color: Color(0xFF3254AB)),
-                  label: 'Profil',
-                ),
-              ],
-              selectedItemColor: Colors.blue,
-              unselectedItemColor: Colors.grey,
-              showSelectedLabels: true,
-              showUnselectedLabels: true,
-              elevation: 0,
-              backgroundColor: Colors.white,
-            ),
-          ),
-        ));
+      ),
+    );
   }
 }
 
@@ -164,64 +165,16 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   void _navigateToPage(BuildContext context, String pageTitle) {
-    switch (pageTitle) {
-      case 'Cari \nDokter':
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => CariDokterPage()),
-        );
-        break;
-      case 'Tanya \nDokter':
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => TanyaDokterPage()),
-        );
-        break;
-      case 'Artikel \nKesehatan':
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ArtikelKesehatanPage()),
-        );
-        break;
-      case 'Pesan \nDriver':
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ProfileUpdateScreen()),
-        );
-        break;
-      case 'Sliva \nProteksi':
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ProfileUpdateScreen()),
-        );
-        break;
-      case 'Sliva \nShop':
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => SlivaShopPage()),
-        );
-        break;
-      case 'Animal \nEvent':
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => AnimalEventPage()),
-        );
-        break;
-      case 'Animal \nAcademy':
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => AnimalAcademyPage()),
-        );
-        break;
-      default:
-        break;
-    }
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SliderBanner()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 30.0),
+      padding: const EdgeInsets.symmetric(vertical: 30.0),
       child: SingleChildScrollView(
         child: Column(
           children: [
@@ -244,6 +197,8 @@ class HomeScreen extends StatelessWidget {
                 _buildButton(context, 'Animal \nAcademy'),
               ],
             ),
+            const SizedBox(height: 20.0),
+            const SliderBanner()
           ],
         ),
       ),
